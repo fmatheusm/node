3cae5f90a10cfed55d03ad9802384f7a0557fc8b
Matheus Martins <matheuss_fernandes@outlook.com>
2025-05-02 21:20:28 -0300

    .

---
+++ modulos2/app.js
@@ -0,0 +1,13 @@
+// const mutiplication = require('./mod1');
+
+// console.log(mutiplication(2, 3)); // 6
+
+// const Cachorro = require('./mod1');
+// const cachorro1 = new Cachorro('Rex');
+// cachorro1.latir(); // Rex está latindo!
+
+// console.log(__filename);
+// console.log(__dirname);
+
+const path = require('path');
+console.log(path.resolve(__dirname, '..', '..'));
