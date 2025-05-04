36c30afb3152706b62e57becbc75d03f04a71f6a
Matheus Martins <matheuss_fernandes@outlook.com>
2025-05-04 19:45:48 -0300

    file system

---
+++ 128ListandoArquivoFsRecursaoMutua/index.js
@@ -0,0 +1,33 @@
+const fs = require('fs').promises;
+const path = require('path');
+
+// fs.readdir(path.resolve(__dirname))
+//     .then(files => { console.log(files) })
+//     .catch(e => { console.error(e) });
+
+async function readdir(rootDir) {
+    rootDir = rootDir || path.resolve(__dirname);
+    const files = await fs.readdir(rootDir);
+    walk(files, rootDir);
+}
+
+async function walk(files, rootDir) {
+    for (const file of files) {
+        const fileFullPath = path.resolve(rootDir, file);
+        const stats = await fs.stat(fileFullPath);
+
+        if (/\.git/g.test(fileFullPath)) continue;
+        if (/node_module/g.test(fileFullPath)) continue;
+
+        if (stats.isDirectory()) {
+            readdir(fileFullPath);
+            continue;
+        }
+
+        if (!/\.css$/g.test(fileFullPath)) continue;
+
+        console.log(file, stats.isDirectory());
+    }
+}
+
+readdir('/dev/node/');
