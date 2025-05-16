d981166218dd0c50f10dba6064d1ad910b510adb
Matheus Martins <matheuss_fernandes@outlook.com>
2025-05-15 22:22:09 -0300

    modelo MVC com arquivo server.js comentado

--- modeloMVC/server.js
+++ modeloMVC/server.js
@@ -21,7 +21,7 @@ const { middlewareGlobal, checkCsurfError, csrfMiddleware } = require('./src/mid
 app.use(helmet()); // Usa o helmet para proteger o aplicativo definindo cabeçalhos HTTP apropriados
 app.use(express.urlencoded({ extended: true })); // Permite que o Express entenda dados de formulários (application/x-www-form-urlencoded)
 app.use(express.json()); // Permite que o Express entenda JSON no corpo das requisições
-app.use(express.static(path.resolve(__dirname, 'public')));
+app.use(express.static(path.resolve(__dirname, 'public'))); // Define o diretório público para arquivos estáticos (CSS, JS, imagens, etc.)
 
 const sessionOptions = session({
     secret: 'teste*r#2sadf',
@@ -32,17 +32,17 @@ const sessionOptions = session({
         maxAge: 1000 * 60 * 60 * 24 * 7,
         httpOnly: true
     }
-});
-app.use(sessionOptions);
-app.use(flash());
+});// Configurações da sessão
+app.use(sessionOptions); // Usa o middleware de sessão com as opções definidas
+app.use(flash()); // Usa o middleware de flash para mensagens temporárias
 
-app.set('views', path.resolve(__dirname, 'src', 'views'));
-app.set('view engine', 'ejs');
+app.set('views', path.resolve(__dirname, 'src', 'views'));// Define o diretório de views (páginas HTML) do aplicativo
+app.set('view engine', 'ejs'); // Define o mecanismo de template como EJS
 
-app.use(csurf())
-app.use(middlewareGlobal);
-app.use(checkCsurfError);
-app.use(csrfMiddleware);
+app.use(csurf()); // Usa o middleware csurf para proteção contra CSRF
+app.use(middlewareGlobal); // Usa o middleware global para definir variáveis de resposta e mensagens de flash
+app.use(checkCsurfError); // Usa o middleware para verificar erros de CSRF
+app.use(csrfMiddleware); // Usa o middleware para definir o token CSRF nas variáveis de resposta
 app.use(routes); // Usando o arquivo de rotas
 
 
